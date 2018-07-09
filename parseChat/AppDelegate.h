32cb162aa5f47f0aeacf6e71ae9d5408de91869e
Gustavo Coutinho <guscoutinho@fb.com>
2018-07-09 10:18:00 -0700

    Initial Commit

---
+++ parseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  parseChat
+//
+//  Created by Gustavo Coutinho on 7/9/18.
+//  Copyright © 2018 Gustavo Coutinho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
